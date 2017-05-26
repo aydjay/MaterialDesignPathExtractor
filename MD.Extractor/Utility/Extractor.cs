c45079ba10489d821338a8208d5890c19e008260
aydjay <aydjay@gmail.com>
2017-05-26 10:52:01 +0100

    Added ability to pull path from SVG files

--- MD.Extractor/Utility/Extractor.cs
+++ MD.Extractor/Utility/Extractor.cs
@@ -1,21 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace MD.Extractor.Utility
 {
     public class Extractor
     {
-        public string GetScalarFromSvg(string accountSvgPath)
+        public string GetVectorFromSvg(string file)
         {
-            if (accountSvgPath.EndsWith(".svg") == false)
-            {
+            if (file.EndsWith(".svg") == false)
                 throw new InvalidOperationException("Must be an SVG file.");
+
+            if (File.Exists(file) == false)
+                throw new FileNotFoundException();
+
+
+            var doc = XDocument.Load(file);
+            var root = doc.Root;
+            var path = root.Descendants("{http://www.w3.org/2000/svg}path");
+
+            if (path.Count() > 1)
+            {
+                Debug.WriteLine("${file} has more than one path.");
+            }
+            else
+            {
+                return path.First().Attribute("d").Value;
+            }
+           
+            return "";
+        }
+
+        public string GetGeometryFromSvg(string key, string vector)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (vector == null) throw new ArgumentNullException(nameof(vector));
+
+            TextInfo textInfo = new CultureInfo("en-GB", false).TextInfo;
+            if (key.Contains("-"))
+            {
+                var items = key.Split('-');
+                items.ToList().ForEach(x => textInfo.ToTitleCase(x));
+                key = string.Join("", items);
+            }
+            else
+            {
+                key = textInfo.ToTitleCase(key);
             }
 
-            return "";
+            var element = new XElement("Geometry", vector);
+            element.Add(new XAttribute("Key", key + "Path"));
+
+            //Todo: get the xaml attribute to build properly instead of this hack
+            return element.ToString().Replace("Key=\"", "x:Key=\"");
         }
     }
 }
--- MD.Tests/ExtractorTests.cs
+++ MD.Tests/ExtractorTests.cs
@@ -7,18 +7,36 @@ namespace MD.Tests
     [TestClass]
     public class ExtractorTests
     {
+        private string accountSvgPath;
+        private Extractor.Utility.Extractor extractor;
+
+        [TestInitialize]
+        public void TestInitialise()
+        {
+            accountSvgPath = Path.GetFullPath(@"..\..\..\MD.Extractor\Raw SVG\account.svg");
+            extractor = new Extractor.Utility.Extractor();
+
+            Assert.IsTrue(File.Exists(accountSvgPath));
+        }
+
         [TestMethod]
         public void CanSvgBeExtracted()
         {
-            var accountSvgPath = Path.GetFullPath(@"..\..\..\MD.Extractor\Raw SVG\account.svg");
+            var result = extractor.GetVectorFromSvg(accountSvgPath);
 
-            Assert.IsTrue(File.Exists(accountSvgPath));
+            var desiredData =
+                "M 12,4C 14.2091,4 16,5.79086 16,8C 16,10.2091 14.2091,12 12,12C 9.79086,12 8,10.2091 8,8C 8,5.79086 9.79086,4 12,4 Z M 12,14C 16.4183,14 20,15.7909 20,18L 20,20L 4,20L 4,18C 4,15.7909 7.58172,14 12,14 Z ";
 
-            Extractor.Utility.Extractor extractor = new Extractor.Utility.Extractor();
+            Assert.AreEqual(desiredData, result);
+        }
 
-            var result = extractor.GetScalarFromSvg(accountSvgPath);
-
-            string desiredData = "M 12,4C 14.2091,4 16,5.79086 16,8C 16,10.2091 14.2091,12 12,12C 9.79086,12 8,10.2091 8,8C 8,5.79086 9.79086,4 12,4 Z M 12,14C 16.4183,14 20,15.7909 20,18L 20,20L 4,20L 4,18C 4,15.7909 7.58172,14 12,14 Z";
+        [TestMethod]
+        public void IsGeometryBuiltCorrectly()
+        {
+            var result = extractor.GetGeometryFromSvg("account",
+                "M 12,4C 14.2091,4 16,5.79086 16,8C 16,10.2091 14.2091,12 12,12C 9.79086,12 8,10.2091 8,8C 8,5.79086 9.79086,4 12,4 Z M 12,14C 16.4183,14 20,15.7909 20,18L 20,20L 4,20L 4,18C 4,15.7909 7.58172,14 12,14 Z ");
+            var desiredData =
+                "<Geometry x:Key=\"AccountPath\">M 12,4C 14.2091,4 16,5.79086 16,8C 16,10.2091 14.2091,12 12,12C 9.79086,12 8,10.2091 8,8C 8,5.79086 9.79086,4 12,4 Z M 12,14C 16.4183,14 20,15.7909 20,18L 20,20L 4,20L 4,18C 4,15.7909 7.58172,14 12,14 Z </Geometry>";
 
             Assert.AreEqual(desiredData, result);
         }
@@ -29,9 +47,20 @@ namespace MD.Tests
         {
             var accountSvgPath = Path.GetFullPath(@"C:\Blahblahblah.txt");
 
-            Extractor.Utility.Extractor extractor = new Extractor.Utility.Extractor();
+            var extractor = new Extractor.Utility.Extractor();
 
-            var result = extractor.GetScalarFromSvg(accountSvgPath);
+            var result = extractor.GetVectorFromSvg(accountSvgPath);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void MustSupplyConcreteFile()
+        {
+            var accountSvgPath = Path.GetFullPath(@"C:\Blahblahblah.svg");
+
+            var extractor = new Extractor.Utility.Extractor();
+
+            var result = extractor.GetVectorFromSvg(accountSvgPath);
         }
     }
 }
